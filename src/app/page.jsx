ca4f60a6a238ea183b821833f108d0e12c621a25
omkar <omkarmore0702@gmail.com>
2024-03-03 16:03:36 +0530

    HERO Section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,31 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+      {/* IMAGE CONTAINER */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+        <Image
+          src="/hero.png"
+          alt="heroImage"
+          fill
+          className="object-contain"
+        />
+      </div>
+
+      {/* TEXT CONTAINER */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+        {/* TITLE */}
+        <h1 className="text-4xl md:text-6xl font-bold ">Crafting Digital Experience</h1>
+        <p className="md:text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta dolor quam ullam veritatis sapiente numquam commodi culpa dolorum magni id.</p>
+        {/* BUTTONS */}
+        <div className="w-full flex gap-4">
+          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View My Work</button>
+          <button className="p-4 rounded-lg ring-1 ring-black ">Contact Me</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
