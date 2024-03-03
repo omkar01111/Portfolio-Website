9e3be4370bb8872c01cab2e356afde0156e1eae5
omkar <omkarmore0702@gmail.com>
2024-03-03 11:59:33 +0530

    Navabar creation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 "use client";
+import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
+import NavLink from "./NavLink";
 const links = [
   { url: "/", title: "Home" },
   { url: "/about", title: "About" },
@@ -11,9 +13,15 @@ const links = [
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <div className="h-full flex items-center  justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    <div className="h-full flex items-center  justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+      {/* Links */}
+      <div className="hidden md:flex gap-5 w-1/3">
+        {links.map((link) => (
+          <NavLink link={link} key={link.title} />
+        ))}
+      </div>
       {/* Logo */}
-      <div className="">
+      <div className="md:hidden lg:flex xl:justify-center xl:w-1/3">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center "
@@ -24,11 +32,29 @@ const Navbar = () => {
           </span>
         </Link>
       </div>
+      {/* SOCIAL */}
+      <div className="hidden md:flex  gap-4 w-1/3 justify-center">
+        <Link href={"/"}>
+          <Image src="/github.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href={"/"}>
+          <Image src="/instagram.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href={"/"}>
+          <Image src="/linkedin.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href={"/"}>
+          <Image src="/leetcode.png" alt="" width={24} height={24} />
+        </Link>
+      </div>
 
       {/* RESPONSIVE MENU */}
-      <div className="">
+      <div className="md:hidden">
         {/* MENU BUTTON */}
-        <button className="w-10 h-8 flex flex-col justify-between z-50 relative ">
+        <button
+          className="w-10 h-8 flex flex-col justify-between z-50 relative "
+          onClick={() => setOpen((prev) => !prev)}
+        >
           <div className="w-10 h-1 bg-white rounded"></div>
           <div className="w-10 h-1 bg-white rounded"></div>
           <div className="w-10 h-1 bg-white rounded"></div>
